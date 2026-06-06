84cd88ccb9cd45703f808eb7f554cd93ad59953d
Abhijeet <pikupc01@gmail.com>
2026-06-07 00:48:38 +0530

    fix the domain mismatch issue

--- server/src/utils/cookieOptions.js
+++ server/src/utils/cookieOptions.js
@@ -5,7 +5,7 @@ const cookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   domain:
     process.env.NODE_ENV === "production"
-      ? ".tokenmitra.online"
+      ? ".abhijeetghosh.site"
       : undefined,
 };
 
